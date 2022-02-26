3f4b51d3b8464c7bf7ad17d3c29ebc3c4d3bd80c
hydrogen_deuteride <dumpsterowner@gmail.com>
2022-02-26 03:03:11 +0000

    add annotations, IOs, etc

--- gameoflife.hs
+++ gameoflife.hs
@@ -1,10 +1,14 @@
 import Data.Set as Set
 import Data.List
+import Control.Concurrent
+
+type Pos = (Int, Int)
+type Board = Set Pos    --set can check double indexing easily 
 
 row = 30
 col = 50
 
---initial data 
+--initial data parsing
 bData::[Char]
 bData = "\
     \..................................................\
@@ -22,60 +26,61 @@ bData = "\
     \..................................................\
     \..................................................\
     \..................................................\
+    \......XX..........................................\
+    \......XX..........................................\
+    \........XX........................................\
+    \........XX........................................\
+    \..................................................\
+    \..................................................\
+    \.......................X..........................\
+    \........................X.........................\
+    \......................XXX.........................\
     \..................................................\
     \..................................................\
     \..................................................\
     \..................................................\
     \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \..................................................\
-    \.................................................."
+    \...............................................…"
 
 pos1D :: [Char] -> [Int]
-pos1D = elemIndices 'X'
+pos1D = elemIndices 'X' -- find position of 'X'
 
 pos1DT2D :: [Int] -> [Pos]
-pos1DT2D zs = [(z `div` col, z `mod` col) | z <- zs]
+pos1DT2D zs = [(z `mod` col, z `div` col) | z <- zs]  --change position type to Int -> (Int, Int)
 
 pos2Set :: [Pos] -> Board
 pos2Set = Set.fromList 
 
--- ////////////////////////////////////////////////////////////////////////////
-type Pos = (Int, Int)
-type Board = Set Pos
+parser:: Board
+parser = Set.fromList (pos1DT2D (elemIndices 'X' bData)) 
+
+-- game processing code
 
 isAlive :: Board -> Pos -> Bool
 isAlive b p = member p b
 
 inBoard :: Int -> Int -> Bool
-inBoard x y = (x `elem` [0..row]) && ( y `elem` [0..col])
+inBoard x y = (x `elem` [0..col]) && ( y `elem` [0..row])  -- check cell is in game processing area
 
 neighbs::Pos -> Board
 neighbs (x, y) = Set.fromList(Prelude.filter (uncurry inBoard)
-    [(x + x', y + y') | x' <- [-1..1], y' <- [-1..1], x' /= 0 || x' -y' /= 0])
+    [(x + x', y + y') | x' <- [-1..1], y' <- [-1..1], x' /= 0 || x' - y' /= 0])  -- neighbor cell check,   x'/= 0 -> y= any ,, x' = 0 -> y /= 0 
 
 liveNeighbs :: Board -> Pos -> Int
-liveNeighbs b p = Set.size (Set.filter (isAlive b) (neighbs p))
+liveNeighbs b p = Set.size (Set.filter (isAlive b) (neighbs p)) 
 
 cell::Board -> Pos -> Bool
 cell b p
-    | isAlive b p = liveNeighbs b p `elem` [2, 3]
-    | otherwise = liveNeighbs b p == 3
+    | isAlive b p = liveNeighbs b p `elem` [2, 3]               --live cell
+    | otherwise = liveNeighbs b p == 3                          --dead cell birth
 
 renderArea :: Board -> Board
-renderArea b = Set.union b $ unions [neighbs x | x <-  toList b]
+renderArea b = Set.union b $ unions [neighbs x | x <-  toList b]    --calculate live cells and its neighbors to decrease required processing power
 
 nextGen :: Board -> Board
 nextGen b = Set.filter (cell b) (renderArea b)
 
---IO
+--IO (https://github.com/rst0git/Game-of-Life-Haskell)
 seqn :: [IO a] -> IO ()
 seqn [] = return ()
 seqn (x:xs) = do x
@@ -98,4 +103,8 @@ life :: Board -> IO()
 life b = do
     cls
     showCells b
+    threadDelay 300000  --0.3s time delay
     life (nextGen b)
+
+main :: IO ()
+main = life parser
